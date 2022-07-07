bebdc58e60d39a1074a006a2a026661c0d19089e
MohitSaini <mailtomohit2002@gmail.com>
2022-07-07 17:07:59 +0530

    changed middlewares name to middleware

---
+++ backend/middleware/fetchuser.js
@@ -0,0 +1,23 @@
+var jwt = require('jsonwebtoken');
+const JWT_SECRET = 'mohitcreatinglostandfound';
+
+const fetchuser=(req,res,next)=>{
+    const token=req.header('auth-token');
+    if(!token){
+        res.status(401).send({ error: "Please Authenicate using a valid token" })
+    }
+
+    try {
+        //! verify will return the payload or the data that we sent while creating our user
+
+        const data = jwt.verify(token, JWT_SECRET);
+
+        // 
+        req.user = data.user;
+        next();
+
+    } catch (error) {
+        res.status(401).send({ error: "Please Authenicate using a valid token" })
+    }
+}
+module.exports = fetchuser;
