38ebc8ad96f830e4be11f921ec8b91744bf1ecda
MohitSaini <mailtomohit2002@gmail.com>
2022-07-25 13:27:16 +0530

    project changed

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,16 +3,17 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCJbKF_-ined5VjVXD3NX4iykjY_0tYFIk",
-    authDomain: "lostandfound-86eb6.firebaseapp.com",
-    projectId: "lostandfound-86eb6",
-    storageBucket: "lostandfound-86eb6.appspot.com",
-    messagingSenderId: "934400339029",
-    appId: "1:934400339029:web:c944a97fe139560c4ff8dc",
-    measurementId: "G-JS1XDLWVM7"
+
+    apiKey: "AIzaSyDyViedX7kcS6SLS6zKJD09zJEcrsKUMFE",
+    authDomain: "lostandfoundiiitdmj.firebaseapp.com",
+    projectId: "lostandfoundiiitdmj",
+    storageBucket: "lostandfoundiiitdmj.appspot.com",
+    messagingSenderId: "46725157701",
+    appId: "1:46725157701:web:5683c7b8613bd5b7bd242c",
+    measurementId: "G-K0QS3HY5LG"
 };
 
 
 export const app = initializeApp(firebaseConfig);
-export const database=getFirestore(app);
+export const database = getFirestore(app);
 const analytics = getAnalytics(app);
